6ed458e682ac992bb77867de161080e2d3bd957e
Shridhara Aithal <vshridhara@gmail.com>
2015-04-21 13:52:28 +0530

    Cosmetic Changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,34 @@
 ## File contains functions that cache a matrix's inverse and return the inverse
 ## instead of calculating it repeatedly
 
-## Create a "matrix object" capable of storing its inverse
+## Usage examples
+## ma <- makeCacheMatrix(1:4, 2, 2)
+## cacheSolve(ma)  #returns the inverse of matrix(1:4, 2, 2) and caches the value
+## ma$setMatrix(matrix(1:4, 2, 2)) #because the new matrix is same as the existing matrix
+##                                 #inverse calculated earlier remains unchanged
+## ma$getInverse() #returns the previously calculated inverse
+## ma$setMatrix(matrix(4:7, 2, 2)) #replaces the matrix and sets inverse to null
+## ma$getInverse() #returns NULL
+## cacheSolve(ma) #computes the inverse of the new matrix and returns the value
 
+
+## Create a "matrix object" capable of caching its inverse
 makeCacheMatrix <- function(x = matrix()) {
-    #stores the inverse
+    # Stores the inverse
     matInv <- NULL
     
-    getMatrix <- function() x
+    # Return the matrix that was used to create the special object
+    getMatrix <- function() 
+    {
+        x
+    }
+    
+    # Assign a new value to the stored matrix. 
+    # Assignment happens only when the new matrix differs from the existing matrix 
     setMatrix <- function(mat)
     {
-        # assign mat to x only if the differ.
-        # if they are identical, preserve the value of matInv
+        # Assign mat to x only if they differ.
+        # If they are identical, preserve the value of matInv
         if(!identical(x, mat))
         {
             x <<- mat
@@ -19,27 +36,46 @@ makeCacheMatrix <- function(x = matrix()) {
         }
     }
     
-    getInverse <- function() matInv
-    setInverse <- function(i) matInv <<- i
+    # Return the stored value of the matrix inverse
+    getInverse <- function() 
+    {
+        matInv
+    }
     
+    # Set the value of matrix inverse
+    setInverse <- function(i) 
+    {
+        matInv <<- i
+    }
     
+    # Return a list of various objects created
     list(getMatrix = getMatrix, setMatrix = setMatrix, 
          getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## computes the inverse of a matrix if it is not cached and returns the inverse
-
+## Computes the inverse of a matrix if it is not cached and returns the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Return a matrix that is the inverse of 'x'
+    
+    # Get the value of the matrix inverse. If it is computed return it.
     invr <- x$getInverse()
     if(!is.null(invr))
     {
         return(invr)
     }
     
+    # Following lines are executed only when matrix inverse is not computed yet.
+    
+    # Retrieve the matrix from the matrix object created using makeCacheMatrix
     mat <- x$getMatrix()
+    
+    # Compute Inverse
     invr <- solve(mat, ...)
+    
+    # Set the value of the inverse in the matrix object
     x$setInverse(invr)
+    
+    # Return the inverse
     invr
 }
